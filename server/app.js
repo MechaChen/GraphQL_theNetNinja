18a623f993e18579e39e8aaa1466f33dd9e8505e
Benson <tumlivein@gmail.com>
2021-02-11 22:53:38 +0800

    #16 - Connecting to mLab

--- server/app.js
+++ server/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
 
 const app = express()
+
+// connect to mongodb atlas
+// make sure to replace my db string & creds with your own
+mongoose.connect('')
+mongoose.connection.once('open', () => console.log('connected to database'))
 
 app.use(
     '/graphql',
