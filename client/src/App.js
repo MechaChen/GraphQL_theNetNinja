7417060ab9c8fcd6799a4ef1375352cc7b95a97a
Benson <tumlivein@gmail.com>
2021-02-13 20:04:04 +0800

    #25 - Apollo Client Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import { ApolloClient, ApolloProvider } from '@apollo/client'
+
+// components
+import BookList from './components/BookList'
+
+// apollo client setup
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+})
+
 function App() {
     return (
-        <div className="App">
-            <h1>Ninja Reading List</h1>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <h1>Ninja Reading List</h1>
+                <BookList />
+            </div>
+        </ApolloProvider>
     )
 }
 
