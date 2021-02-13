8482ed9a9da73e8b68b06e13bdb50f7c0dd7fefd
Benson <tumlivein@gmail.com>
2021-02-13 21:46:32 +0800

    #26 - Making Queries from React

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -1,4 +1,19 @@
+import { useQuery, gql } from '@apollo/client'
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
 function BookList() {
+    const { loading, error, data } = useQuery(getBooksQuery)
+
+    console.log('data =>', data)
+
     return (
         <div>
             <ul id="book-list">
